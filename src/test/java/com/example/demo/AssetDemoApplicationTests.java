e0b6f407e0549d64d43d4517b537fd83a307c9ad
David Turanski <dturanski@pivotal.io>
2017-05-23 21:02:49 -0400

    Fix tests. Add SAML

--- src/test/java/com/example/demo/AssetDemoApplicationTests.java
+++ src/test/java/com/example/demo/AssetDemoApplicationTests.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest("eureka.client.enabled=false")
+@SpringBootTest({
+			"eureka.client.enabled=false",
+			"debug=true"})
 public class AssetDemoApplicationTests {
 
 	@Test
--- src/test/java/com/example/demo/reposiitory/AssetsRepositoryTests.java
+++ src/test/java/com/example/demo/reposiitory/AssetsRepositoryTests.java
@@ -5,7 +5,10 @@ import com.example.demo.repository.AssetsRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  **/
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.HSQL)
 public class AssetsRepositoryTests {
 	@Autowired
 	AssetsRepository assetsRepository;
